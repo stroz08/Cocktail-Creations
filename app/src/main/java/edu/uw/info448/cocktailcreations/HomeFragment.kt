fe727cfc4c1b0ccb46fc51f6992a6549b93c5851
Siena South-Ciero <sienasc@uw.edu>
2021-12-01 11:11:09 -0800

    started navigation bar

---
+++ app/src/main/java/edu/uw/info448/cocktailcreations/HomeFragment.kt
@@ -0,0 +1,23 @@
+//Siena South-Ciero created the home fragment
+package edu.uw.info448.cocktailcreations
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class HomeFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+}
