f8cf496a700b7e9f12b97abfdde73ead4afbe700
Sarah West <smwest3@uw.edu>
2021-12-11 14:22:18 -0800

    login and camera activities created, login layouts complete

---
+++ app/src/main/java/edu/uw/info448/cocktailcreations/CameraActivity.kt
@@ -0,0 +1,12 @@
+package edu.uw.info448.cocktailcreations
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CameraActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+    }
+}
---
+++ app/src/main/java/edu/uw/info448/cocktailcreations/LoginFragment.kt
@@ -0,0 +1,7 @@
+package edu.uw.info448.cocktailcreations
+
+import androidx.fragment.app.Fragment
+
+class LoginFragment: Fragment() {
+
+}
