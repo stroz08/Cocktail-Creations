0ddc16e0d733c8b2ad5a295ef21c236eed784a62
Siena South-Ciero <sienasc@uw.edu>
2021-12-12 15:26:47 -0800

    added cocktailname and img to recipe card

--- app/src/main/java/edu/uw/info448/cocktailcreations/CocktailListAdapter.kt
+++ app/src/main/java/edu/uw/info448/cocktailcreations/CocktailListAdapter.kt
@@ -14,6 +14,7 @@ import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import edu.uw.info448.cocktailcreations.network.Cocktail
 
 private const val TAG = "CocktailListAdapter"
@@ -35,7 +36,6 @@ class CocktailListAdapter() : ListAdapter<Cocktail, CocktailListAdapter.ViewHold
         val cocktailName = item.name
 
         holder.cocktailName.text = item!!.name
-
 
         //handle navigation
         holder.cocktailName.setOnClickListener {
--- app/src/main/java/edu/uw/info448/cocktailcreations/RecipeCard.kt
+++ app/src/main/java/edu/uw/info448/cocktailcreations/RecipeCard.kt
@@ -7,18 +7,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 
-class RecipieCardFragment : Fragment() {
+class RecipeCardFragment : Fragment() {
     private var cocktailName: String? = null
     private var cocktailImg: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val args: RecipieCardFragmentArgs by navArgs()
+        val args: RecipeCardFragmentArgs by navArgs()
         cocktailName = args.cocktailName
         cocktailImg = args.cocktailImg
     }
@@ -27,7 +26,7 @@ class RecipieCardFragment : Fragment() {
         // Inflate the layout for this fragment
         val rootView = inflater.inflate(R.layout.fragment_recipe_card, container, false)
         rootView.findViewById<TextView>(R.id.recipeCardName).text = cocktailName
-        //Glide.with(this).load("$cocktailImg").into(rootView.findViewById(R.id.cocktailListImg))
+        Glide.with(this).load("$cocktailImg").into(rootView.findViewById(R.id.recipeCardImg))
         return rootView
     }
 }
