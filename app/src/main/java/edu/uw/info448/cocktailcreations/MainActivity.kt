adba6349a4f20087ed9d26f93f0410f85ae9525f
Sarah West <smwest3@uw.edu>
2021-12-13 19:40:19 -0800

    logout button added

--- app/src/main/java/edu/uw/info448/cocktailcreations/MainActivity.kt
+++ app/src/main/java/edu/uw/info448/cocktailcreations/MainActivity.kt
@@ -1,13 +1,19 @@
 //Siena South-Ciero worked on the navigation
 package edu.uw.info448.cocktailcreations
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -16,11 +22,18 @@ class MainActivity : AppCompatActivity() {
     private val searchFragment = SearchFragment()
     private val randomFragment = RandomFragment()
     private val profileFragment = ProfileFragment()
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         replaceFrag(homeFragment)
+        auth = Firebase.auth
+
+        //logout button
+        findViewById<Button>(R.id.logout_button).setOnClickListener {
+            logoutRedirect()
+        }
 
         //navigation
         val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
@@ -44,5 +57,14 @@ class MainActivity : AppCompatActivity() {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.nav_fragment, fragment)
         transaction.commit()
+    }
+
+    private fun logoutRedirect() {
+        if (auth.currentUser != null) {
+            auth.signOut()
+            startActivity(Intent(this, LoginActivity::class.java))
+        } else {
+            Toast.makeText(baseContext, "Error: no user data found", Toast.LENGTH_SHORT).show()
+        }
     }
 }
